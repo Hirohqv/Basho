5bb92977782e40e28e1fd8ef4611df946540ab37
Hiro <Hirohqvu@gmail.com>
2022-10-02 15:35:48 +0200

    Commented out meny to make it clearn, added about mdx, going to work and make it work

--- consts.ts
+++ consts.ts
@@ -1,26 +1,29 @@
-import { GitHub, Twitter, LinkedIn } from "@mui/icons-material";
+import { GitHub, Twitter, LinkedIn, Instagram } from "@mui/icons-material";
 export const NAVIGATION = {
-    main: [
-        { name: "Home", href: "/"},
-        { name: "Articles", href: "/"},
-        { name: "About", href: "/"}
-    ],
-    social: [
-        {
-            href: "https://www.linkedin.com/in/hirohqv",
-            muiIcon: LinkedIn
-        },
-        {
-            href: "https://twitter.com/HuyQV",
-            muiIcon: Twitter
-        },
-        {
-            href: "https://github.com/Hirohqv",
-            muiIcon: GitHub
-        }
-    ] 
-}
-export const siteTitle = "Huy Vu"
-export const avatarUrl = "/static/img/avatar.jpeg"
-
-
+  main: [
+    /**{ name: "Home", href: "/" } ,
+    { name: "Articles", href: "/" },
+    { name: "About", href: "/" },
+     */,
+  ],
+  social: [
+    {
+      href: "https://www.linkedin.com/in/hirohqv",
+      muiIcon: LinkedIn,
+    },
+    {
+      href: "https://twitter.com/HuyQV",
+      muiIcon: Twitter,
+    },
+    {
+      href: "https://github.com/Hirohqv",
+      muiIcon: GitHub,
+    },
+    {
+      href: "https://www.instagram.com/huyq.v/",
+      muiIcon: Instagram,
+    },
+  ],
+};
+export const siteTitle = "Huy Vu";
+export const avatarUrl = "/static/img/avatar.jpeg";
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,6 @@ const Home: NextPage = () => {
         <ThingsIDo />
         <Clients />
       </main>
-
       <Footer />
     </div>
   );
